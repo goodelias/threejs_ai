c9b29eb3bdaefd521367a8c9acf20935d9e86eb3
Ilia Cepchin <ilia@ilias-mbp.home>
2023-07-27 12:35:08 +0100

    first commit

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => (state.color = color.hex)}
+        presetColors={[
+          "#ccc",
+          "#EFBD4E",
+          "#80C670",
+          "#726DE8",
+          "#353934",
+          "#2CCCE4",
+          "#FF8A65",
+          "#7098DA",
+          "#C19277",
+          "#FF96AD",
+          "#512314",
+          "#5F123D",
+        ]}
+      />
+    </div>
+  );
+};
+
+export default ColorPicker;
